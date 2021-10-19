8ac7606622f841f3641fc5b1d2b79583a7210cc4
HanJiWoong <hanjw8410@gmail.com>
2021-10-19 13:16:07 +0900

    init

---
+++ Shared/FollinicIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  FollinicIOSApp.swift
+//  Shared
+//
+//  Created by 한지웅-PC on 2021/10/19.
+//
+
+import SwiftUI
+
+@main
+struct FollinicIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
