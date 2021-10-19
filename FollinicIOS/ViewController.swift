aaeedd33a3a94688178146339a53a78952fbcb99
HanJiWoong <hanjw8410@gmail.com>
2021-10-19 13:17:34 +0900

    init

---
+++ FollinicIOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FollinicIOS
+//
+//  Created by 한지웅-PC on 2021/10/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
